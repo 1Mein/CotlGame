5cf5be991ccec6db4aa7bf7ed3e1cf282ee2dc18
1Mein <119441676+1Mein@users.noreply.github.com>
2023-12-20 05:45:21 +0600

    correct out of bonds

--- Objects/Default/Bullet.js
+++ Objects/Default/Bullet.js
@@ -1,14 +1,17 @@
+import { GlobalVariables } from "../GlobalVariables.js";
+
 export class Bullet{
     constructor(){
+        this.gv = new GlobalVariables();
         this.x = 0;
         this.y = 0;
         this.width = 150;
         this.height = 10;
         this.angle = 0;
-        this.speed = 1;
+        this.speed = 10; // speed of bullet movement
         this.isFired = false;
-        this.fireTime = 0; //when bullet will be fired
-        this.color = "#FFF";
+        this.fireTime = 0; // when bullet will be fired
+        this.color = "#FFF"; // color of bullet
 
         this.startX1 = 0;
         this.startY1 = 0;
@@ -38,24 +41,19 @@ export class Bullet{
         ctx.save();
 
         ctx.translate(this.x + this.width / 2, this.y + this.height / 2);
-        ctx.rotate(this.angle + Math.PI / 2);
+        ctx.rotate(this.angle);
 
         ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
 
         ctx.restore();
-        this.setPositions(ctx);
+        this.drawPositions(ctx);
+        this.checkOutOfBounds();
     }
 
 
 
 ////////////////////////////////////////////////////////////////
-    setPositions(ctx) {
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,4,0,this.angle);
-        // console.log(this.angle)
-        ctx.stroke();
-
-
+    drawPositions(ctx) {
         const centerX = this.x + this.width / 2;
         const centerY = this.y + this.height/2;
 
@@ -64,15 +62,15 @@ export class Bullet{
         ctx.stroke();
 
 
-        this.currX1 = centerX + (this.width / 2) * Math.cos(this.angle+ Math.PI / 2);
-        this.currY1 = centerY + (this.width / 2) * Math.sin(this.angle+ Math.PI / 2);
+        this.currX1 = centerX + (this.width / 2) * Math.cos(this.angle);
+        this.currY1 = centerY + (this.width / 2) * Math.sin(this.angle);
 
         ctx.beginPath();
         ctx.arc(this.currX1,this.currY1,1,0,2*Math.PI);
         ctx.stroke();
 
-        this.currX2 = centerX + (-this.width / 2) * Math.cos(this.angle+ Math.PI / 2);
-        this.currY2 = centerY + (-this.width / 2) * Math.sin(this.angle+ Math.PI / 2);
+        this.currX2 = centerX + (-this.width / 2) * Math.cos(this.angle);
+        this.currY2 = centerY + (-this.width / 2) * Math.sin(this.angle);
 
         ctx.beginPath();
         ctx.arc(this.currX2,this.currY2,1,0,2*Math.PI);
@@ -85,4 +83,35 @@ export class Bullet{
         ctx.arc(this.startX2,this.startY2,1,0,2*Math.PI);
         ctx.stroke();
     }
+
+
+    ////////////////////////
+    checkOutOfBounds(){
+        //set positions
+        const X1 = this.currX1;
+        const Y1 = this.currY1;
+        const X2 = this.currX2;
+        const Y2 = this.currY2;
+
+        const X3 = X1;
+        const Y3 = Y2;
+        
+        const X4 = X2;
+        const Y4 = Y1;
+
+        let points = [[X1,Y1], [X2,Y2], [X3,Y3], [X4,Y4]];
+
+        let counter = 0;
+
+        points.forEach(point => {
+            if (point[1] < 0 || point[0] < 0 || point[0] > this.gv.boxWidth || point[1] > this.gv.boxHeight) {
+                counter++;
+            }
+        });
+
+        if(counter === 4){
+            this.isFired = false;
+        }
+    }
 }
+
