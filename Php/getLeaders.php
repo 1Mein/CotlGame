ccf6141c566311f9cb6c8c32053091da3fac5180
1Mein <119441676+1Mein@users.noreply.github.com>
2023-12-22 12:24:21 +0600

    ordered

--- Php/getLeaders.php
+++ Php/getLeaders.php
@@ -1,6 +1,6 @@
 <?php
 require_once('connection.php');
-$query = $connection->query("SELECT username,score FROM leaderboard.users");
+$query = $connection->query("SELECT username,score FROM leaderboard.users order by score  desc limit 10");
 if ($query !== false) {
     while ($row = $query->fetch_assoc()) {
         echo "<tr><th>" . $row["username"] . "</th><th>" . $row["score"] . "</th></tr>";
